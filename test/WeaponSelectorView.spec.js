fb05e9ab423e23c9a2be477dfe0f81afe1982f77
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 14:01:49 +0000

    Add CSS classes

--- test/WeaponSelectorView.spec.js
+++ test/WeaponSelectorView.spec.js
@@ -6,15 +6,15 @@ describe('WeaponSelectorView', () => {
 
         it('should render a list of weapons', () => {
             let element = document.createElement('section');
-            element.innerHTML = '<ul></ul><button></button>';
+            element.innerHTML = '<ul class="js-weapon-list"></ul><button class="js-simutator"></button>';
 
             let expectedHTML = `<section>
-                <ul>
+                <ul class="js-weapon-list">
                     <li><button id="rock" class="weapon-button weapon-rock">rock</button></li>
                     <li><button id="paper" class="weapon-button weapon-paper">paper</button></li>
                     <li><button id="scissors" class="weapon-button weapon-scissors">scissors</button></li>
                 </ul>
-                <button></button>
+                <button> class="js-simutator"</button>
             </section>`
 
             let weaponSelectorView = new WeaponSelectorView(element);
