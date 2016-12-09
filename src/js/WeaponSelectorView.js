a5c74d20615b7b4978d177bf4a2fa4db3cce9bb6
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 14:02:26 +0000

    Add constructor

--- src/js/WeaponSelectorView.js
+++ src/js/WeaponSelectorView.js
@@ -1,3 +1,9 @@
 export default class WeaponSelectorView {
-    
+
+    constructor(element) {
+        this.element = element;
+        this._weaponListElem = this.element.getElementsByClassName('js-weapon-list')[0];
+        this._simutatorButtonElem = this.element.getElementsByClassName('js-simutator')[0];
+    }
+
 }
