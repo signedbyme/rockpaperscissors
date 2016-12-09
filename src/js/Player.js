705cd81af4c8ddddbe02790785f29b69744d6867
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 12:23:03 +0000

    Set weapon to null and add getter for it

--- src/js/Player.js
+++ src/js/Player.js
@@ -2,9 +2,14 @@ export default class Player {
 
     constructor(name) {
         this._name = name
+        this._weapon = null;
     }
 
     get name() {
         return this._name;
+    }
+
+    get weapon() {
+        return this._weapon;
     }
 }
