efafdba32b3183d7ccdf79aabe2092b53d37cb4e
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 13:45:25 +0000

    Test render method

--- test/WeaponSelectorView.spec.js
+++ test/WeaponSelectorView.spec.js
@@ -5,9 +5,22 @@ describe('WeaponSelectorView', () => {
     describe('render', () => {
 
         it('should render a list of weapons', () => {
-            let weaponSelectorView = new WeaponSelectorView();
+            let element = document.createElement('section');
+            element.innerHTML = '<ul></ul><button></button>';
+
+            let expectedHTML = `<section>
+                <ul>
+                    <li><button id="rock" class="weapon-button weapon-rock">rock</button></li>
+                    <li><button id="paper" class="weapon-button weapon-paper">paper</button></li>
+                    <li><button id="scissors" class="weapon-button weapon-scissors">scissors</button></li>
+                </ul>
+                <button></button>
+            </section>`
+
+            let weaponSelectorView = new WeaponSelectorView(element);
             weaponSelectorView.render();
-            expect(weaponSelectorView.element.innerHTML).to.equal();
+
+            expect(weaponSelectorView.element.innerHTML).to.equal(expectedHTML);
         });
 
     });
