48ae3f941cd9ff4d317d11522260b95267973d40
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 15:51:18 +0000

    Test returns winning player & weapon

--- test/calculateWinner.spec.js
+++ test/calculateWinner.spec.js
@@ -3,8 +3,8 @@ import calculateWinner from '../src/js/calculateWinner';
 describe('calculateWinner', () => {
 
         it('should return null if a draw', () => {
-            let playerOne = { weapon: 'Rock'};
-            let playerTwo = { weapon: 'Rock'};
+            let playerOne = { weapon: 'rock'};
+            let playerTwo = { weapon: 'rock'};
 
             let result = calculateWinner(playerOne, playerTwo);
 
@@ -12,7 +12,12 @@ describe('calculateWinner', () => {
         });
 
         it('should return the winning player', () => {
+            let playerOne = { weapon: 'rock'};
+            let playerTwo = { weapon: 'paper'};
 
+            let result = calculateWinner(playerOne, playerTwo);
+
+            expect(result.weapon).to.equal(playerTwo.weapon);
         });
 
 });
