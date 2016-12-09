cf73ab0dc4e0bd313a8276f272c8279d30754764
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 16:39:24 +0000

    Add result view when game has been played

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,12 @@ import calculateWinner from '..//calculateWinner';
 
 const playerOne = new Player('You');
 const playerTwo = new Player('Mr.Robot (aka the computer)');
+
 const weaponsElem = document.getElementsByClassName('js-weapons')[0];
 const weaponSelectorView = new WeaponSelectorView(weaponsElem, onWeaponSelectHandler, onSimulatorClickHandler);
+
+const resultElem = document.getElementsByClassName('js-game-result')[0];
+const resultView = new ResultView(resultElem, playerOne, playerTwo, onPlayAgainHandler);
 
 startGame();
 
@@ -23,15 +27,23 @@ function showWeaponSelectorScreen(){
 }
 
 function showResultScreen() {
-    let winningPlayer = calculateWinner(playerOne, playerTwo);
     weaponSelectorView.hide();
+    resultView.winner = calculateWinningPlayer(playerOne, playerTwo);
+    resultView.render();
 }
 
 function onSimulatorClickHandler() {
     playerOne.name = 'Lazy ol\' you';
     playerOne.weapon = randomWeaponSelector();
+    showResultScreen();
 }
 
 function onWeaponSelectHandler(weapon) {
     playerOne.weapon = weapon;
+    showResultScreen();
+}
+
+function onPlayAgainHandler() {
+    resultView.hide();
+    startGame();
 }
