8d607cc30eeaab08da678549b5414e6aa0e46a50
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 14:15:47 +0000

    Use include to match the <li> elements

--- test/WeaponSelectorView.spec.js
+++ test/WeaponSelectorView.spec.js
@@ -8,19 +8,12 @@ describe('WeaponSelectorView', () => {
             let element = document.createElement('section');
             element.innerHTML = '<ul class="js-weapon-list"></ul><button class="js-simutator"></button>';
 
-            let expectedHTML = `<section>
-                <ul class="js-weapon-list">
-                    <li><button id="rock" class="weapon-button weapon-rock">rock</button></li>
-                    <li><button id="paper" class="weapon-button weapon-paper">paper</button></li>
-                    <li><button id="scissors" class="weapon-button weapon-scissors">scissors</button></li>
-                </ul>
-                <button> class="js-simutator"</button>
-            </section>`
+            let expectedHTML = `<li><button id="rock" class="weapon-button weapon-rock">rock</button></li><li><button id="paper" class="weapon-button weapon-paper">paper</button></li><li><button id="scissors" class="weapon-button weapon-scissors">scissors</button></li>`
 
             let weaponSelectorView = new WeaponSelectorView(element);
             weaponSelectorView.render();
 
-            expect(weaponSelectorView.element.innerHTML).to.equal(expectedHTML);
+            expect(weaponSelectorView.element.innerHTML).to.include(expectedHTML);
         });
 
     });
