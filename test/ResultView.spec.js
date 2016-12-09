2bd4a8da186ef61cbc25fa74a9f9383b4018c29f
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 16:29:38 +0000

    Test for a drawn result

--- test/ResultView.spec.js
+++ test/ResultView.spec.js
@@ -4,26 +4,51 @@ describe('ResultView', () => {
 
     describe('render', () => {
 
+        describe('wining result', () => {
 
-        it('should render the result', () => {
-            let element = document.createElement('section');
-            let playerOne = {
-                name: 'Player one',
-                weapon: 'rock'
-            };
-            let playerTwo = {
-                name: 'Player two',
-                weapon: 'paper'
-            };
+            it('should render the winning result message', () => {
+                let element = document.createElement('section');
+                let playerOne = {
+                    name: 'Player one',
+                    weapon: 'rock'
+                };
+                let playerTwo = {
+                    name: 'Player two',
+                    weapon: 'paper'
+                };
 
-            let resultView = new ResultView(element, playerOne, playerTwo);
-            resultView.winner = playerTwo;
-            resultView.render();
+                let resultView = new ResultView(element, playerOne, playerTwo);
+                resultView.winner = playerTwo;
+                resultView.render();
 
-            expect(resultView.element.innerHTML).to.include('And the winner is...Player two!');
-            expect(resultView.element.innerHTML).to.include('paper beats rock');
-            expect(resultView.element.innerHTML).to.include('Player one chose rock');
-            expect(resultView.element.innerHTML).to.include('Player two chose paper');
+                expect(resultView.element.innerHTML).to.include('And the winner is...Player two!');
+                expect(resultView.element.innerHTML).to.include('paper beats rock');
+                expect(resultView.element.innerHTML).to.include('Player one chose rock');
+                expect(resultView.element.innerHTML).to.include('Player two chose paper');
+            });
+        });
+
+        describe('a draw', () => {
+
+            it('should render the draw result message', () => {
+                let element = document.createElement('section');
+                let playerOne = {
+                    name: 'Player one',
+                    weapon: 'rock'
+                };
+                let playerTwo = {
+                    name: 'Player two',
+                    weapon: 'rock'
+                };
+
+                let resultView = new ResultView(element, playerOne, playerTwo);
+                resultView.winner = null;
+                resultView.render();
+
+                expect(resultView.element.innerHTML).to.include('It\'s a draw');
+                expect(resultView.element.innerHTML).to.include('Player one chose rock');
+                expect(resultView.element.innerHTML).to.include('Player two chose rock');
+            });
         });
 
     });
