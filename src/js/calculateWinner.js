b98b193608fbeae402ea89274b1c9bee6fe160fc
Chris Laslett <chrislaslett@gmail.com>
2016-12-09 17:06:03 +0000

    Add missing semi-colon

--- src/js/calculateWinner.js
+++ src/js/calculateWinner.js
@@ -2,7 +2,7 @@ import { weaponRules } from './weapons';
 
 export default function calculateWinner(playerOne, playerTwo){
     if (playerOne.weapon === playerTwo.weapon) {
-        return null
+        return null;
     }
 
     return weaponRules[playerOne.weapon][playerTwo.weapon] ? playerOne : playerTwo;
